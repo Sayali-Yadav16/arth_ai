1e3bece88b1595e58da91a44e99a2d9618a8bf60
ishwari shendkar <147191977+shendkar@users.noreply.github.com>
2026-01-25 07:02:30 +0530

    Resolved merge conflicts after moving frontend to root

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { createUser, findUserByEmail, findUserById } from '../models/userModels.js';
